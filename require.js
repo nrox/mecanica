2726d766733b097bb3780774cde3cdc451b04a81
nrox <nunuroxa@gmail.com>
2014-10-21 20:27:29 +0200

    improvements in require: more clear

--- require.js
+++ require.js
@@ -1,121 +1,13 @@
-var _availableFiles;
-
-/**
- * A custom version of require for this library purpose
- * It was developed, instead of using browserify, because of Ammo.js trying to require fs and path
- * @param script the relative the url
- * @param arg the argument for test scripts
- * @returns {module.exports || undefined}
- */
-function require(script, arg) {
-
-  if (!arg) arg = undefined;
-
-  function fileName(script) {
-    return script.substr(script.lastIndexOf('/') + 1);
-  }
-
-  //some caveats:
-  //listdir.sh should be run whenever the directory structure changes
-  //FIXME: html files with the same name
-  function scriptURL(script) {
-    var url;
-    var scriptFragments = script.split('/');
-    var htmlFile = location.pathname;
-    if (htmlFile.lastIndexOf('/') === (htmlFile.length - 1)) {
-      htmlFile = htmlFile.substr(0, htmlFile.length - 1);
-    }
-    var reconFiles = [];
-    var reconHtml = [];
-    for (var i = 0; i < _availableFiles.length; i++) {
-      var file = _availableFiles[i];
-      if (htmlFile.lastIndexOf(file) >= 0 && ((htmlFile.lastIndexOf(file) + file.length) == htmlFile.length)) {
-        reconHtml.push(file);
-      }
-      var has = 0;
-      for (var f = 0; f < scriptFragments.length; f++) {
-        var frag = scriptFragments[f];
-        if (!frag || frag == '.' || frag == '..') {
-          has++;
-        } else if (file.split('/').indexOf(frag) >= 0) {
-          has++;
-        }
-      }
-      if (has == scriptFragments.length) {
-        reconFiles.push(file);
-      }
-    }
-    if (reconHtml.length != 1 || reconFiles.length != 1) {
-      console.log('html', reconHtml);
-      console.log('files', reconFiles);
-      console.warn('TODO in require : reconHtml.length!=1 || reconFiles.length!=1');
-      console.warn('HINT: run listdir.sh');
-    }
-    url = location.origin + htmlFile.substr(0, htmlFile.lastIndexOf(reconHtml[0])) + reconFiles[0];
-    //console.log(url);
-    return url;
-  }
-
-  function isAmmo(script) {
-    return fileName(script) == 'ammo.js';
-  }
-
-  function isLocalhost() {
-    return location.hostname == 'localhost';
-  }
-
-  if (script.lastIndexOf('.js') < 0) return;
-  var url = scriptURL(script);
-
-  //node.js stubs
-  var process = {
-    //an argument is necessary for some test-....js scripts
-    argv: ['node', script, arg]
-  };
-  var __filename = script;
-  var module = {
-    exports: {}
-  };
-  var exports = module.exports;
-  //in case of ammo remove stubs, or it will require fs and path
-  if (isAmmo(script)) {
-    module = process = exports = undefined;
-  }
-
-  $.ajax(url, {
-    cache: !isLocalhost(), //during development don't cache
-    async: false, //important async: false to return the required object immediately
-    dataType: 'text', //don't run automatically
-    error: function (jqXHR, textStatus, errorThrown) {
-      console.log('error in require ' + script);
-      console.error(errorThrown);
-    },
-    global: false,
-    success: function (data, textStatus, jqXHR) {
-      if (isAmmo(script)) {
-        eval(data);
-        var Ammo = Ammo || {};
-        module = {
-          exports: Ammo
-        };
-      } else
-        eval('(function(){\n' + data + '\n})();');
-    },
-    type: 'GET'
-  });
-  return module.exports;
-}
-
-
 (function () {
-  var url = 'list.js';
+
+  var availablePaths;
+
+  var url = ['../list.js', 'list.js', '../../list.js'];
   var list;
-  for (var i = 0; !list && (i < 5); i++) {
-    $.ajax(url, {
+  for (var i = 0; !list && (i < url.length); i++) {
+    $.ajax(url[i], {
       async: false, //important async: false to return the required object immediately
       dataType: 'text', //don't run automatically
-      error: function () {
-      },
       global: false,
       success: function (data) {
         list = eval(data);
@@ -123,16 +15,127 @@ function require(script, arg) {
           list[i] = list[i].substr(2);
           if (list[i].indexOf('/') === 0) list[i] = list[i].substr(1);
         }
-        _availableFiles = list;
+        availablePaths = list;
       },
       type: 'GET'
-    }).fail(function () {
-        url = '../' + url;
+    });
+  }
+
+
+  /**
+   * A custom version of require for this library purpose
+   * It was developed, instead of using browserify, because of Ammo.js trying to require fs and path
+   * @param script the relative the url
+   * @param arg the argument for test scripts
+   * @returns {module.exports || undefined}
+   */
+  window.require = function (script, arg) {
+
+    if (!arg) arg = undefined;
+
+    function fileName(script) {
+      return script.substr(script.lastIndexOf('/') + 1);
+    }
+
+    function httpRoot() {
+      var locationHref = location.href.split('').reverse().join('');
+      if (locationHref.indexOf('/') === 0) locationHref = locationHref.substr(1);
+      var longest = 0;
+      for (var i = 0; i < availablePaths.length; i++) {
+        if (locationHref.indexOf(availablePaths[i].split('').reverse().join('')) == 0) {
+          if (availablePaths[i].length > longest) {
+            longest = availablePaths[i].length;
+          }
+        }
       }
-    );
+      var root;
+      if (longest) {
+        root = locationHref.substr(longest).split('').reverse().join('');
+      }
+      return root;
+    }
+
+    function requiredPath(script) {
+      script = script.replace('/../', '/');
+      while (script.indexOf('/../') > 0) script = script.replace('/../', '/');
+      if (script.indexOf('./') == 0) script = script.substr(2);
+      if (script.indexOf('../') == 0) script = script.substr(3);
+      while (script.indexOf('//') > 0) script = script.replace('//', '/');
+      while (script.indexOf('/') == 0) script = script.substr(1);
+      var fragments = script.split('/');
+      for (var i = 0; i < availablePaths.length; i++) {
+        var pos = -1;
+        for (var j = 0; j < fragments.length; j++) {
+          pos = availablePaths[i].indexOf(fragments[j], pos);
+        }
+        if (pos > -1) {
+          return availablePaths[i];
+        }
+      }
+    }
+
+    function isAmmo(script) {
+      return fileName(script) == 'ammo.js';
+    }
+
+    function isLocalhost() {
+      return location.hostname == 'localhost';
+    }
+
+    if (script.lastIndexOf('.js') < 0) return;
+    var path = requiredPath(script);
+
+    var url;
+    if (!path) {
+      url = script;
+      console.warning(script + ' is not registered. Use: bash listdir.sh');
+    } else {
+      url = httpRoot() + path;
+    }
+
+    //node.js stubs
+    var process = {
+      //an argument is necessary for some test-....js scripts
+      argv: ['node', script, arg]
+    };
+    var __filename = script;
+    var module = {
+      exports: {}
+    };
+    var exports = module.exports;
+    //in case of ammo remove stubs, or it will require fs and path
+    if (isAmmo(script)) {
+      module = process = exports = undefined;
+    }
+
+    $.ajax(url, {
+      cache: !isLocalhost(), //during development don't cache
+      async: false, //important async: false to return the required object immediately
+      dataType: 'text', //don't run automatically
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log('error in require ' + script);
+        console.error(errorThrown);
+      },
+      global: false,
+      success: function (data, textStatus, jqXHR) {
+        if (isAmmo(script)) {
+          eval(data);
+          var Ammo = Ammo || {};
+          module = {
+            exports: Ammo
+          };
+        } else
+          eval('(function(){\n' + data + '\n})();');
+      },
+      type: 'GET'
+    });
+    return module.exports;
   }
 })();
 
+/**
+ * replace console
+ */
 (function () {
   var $console;
   var backups = {
@@ -178,4 +181,5 @@ function require(script, arg) {
   window.clearConsole = function () {
     $console && $console.empty();
   };
+
 })();
