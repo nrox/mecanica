e5dd8666a34155472d018548aaab48c201c03e29
nrox <nunuroxa@gmail.com>
2015-02-16 16:50:17 +0100

    fix gear constraint test

--- tests/test-constraint.js
+++ tests/test-constraint.js
@@ -92,6 +92,51 @@ var inputs = {
     editor.useTemplate(template);
     editor.setValues(ui);
     editor.showEditor('#triggers');
+  },
+  gear: function (actuator) {
+    actuator = 'motor';
+    var template = {
+      servo: {
+        'angle(°)': {type: 'range', min: -180, max: 180, step: 5}
+      },
+      motor: {
+        velocity: {type: 'range', min: -5, max: 5, step: 1},
+        binary: {type: 'range', min: 0, max: 50, step: 1}
+      }
+    };
+    var ui = {
+      servo: {
+        'angle(°)': 0,
+        set: function () {
+          var c = factory.getObject('constraint', actuator);
+          console.log(c);
+          var angle = this.getValues().servo['angle(°)'];
+          angle = angle * Math.PI / 180;
+          factory.method.constraint.setAngle.call(c, angle);
+        },
+        relax: function () {
+          var c = factory.getObject('constraint', actuator);
+          factory.method.constraint.relax.call(c);
+        }
+      },
+      motor: {
+        velocity: 1,
+        binary: 1,
+        enable: function () {
+          var c = factory.getObject('constraint', actuator);
+          var values = this.getValues().motor;
+          factory.method.constraint.enableMotor.call(c, values.velocity, values.binary);
+        },
+        disable: function () {
+          var c = factory.getObject('constraint', actuator);
+          factory.method.constraint.disableMotor.call(c);
+        }
+      }
+    };
+    var editor = new Editor();
+    editor.useTemplate(template);
+    editor.setValues(ui);
+    editor.showEditor('#triggers');
   }
 };
 
@@ -170,18 +215,115 @@ function addAllTests() {
   bodyBCopy.position = {z: -5, x: 4, y: 5};
   test[type] = makeTest(bodyA, bodyBCopy, ca, cb, type, constraintOptions);
 
-  type = 'gear';
-  ca = utils.deepCopy(connectorA);
-  cb = utils.deepCopy(connectorB);
-  ca.base = {x: -1, y: -1, z: 0};
-  ca.up = {z: 1};
-  cb.base = {x: 1, y: 1, z: 0};
-  cb.up = {z: 1};
-  test[type] = makeTest(bodyA, bodyB, ca, cb, type, constraintOptions);
+}
 
+function gearTest() {
+  var type = 'gear';
+  factory.setScope(type);
+  var pack = {
+    body: {
+      wall: {
+        type: 'basic',
+        shape: { type: 'box', dx: 2, dz: 6, dy: 6, segments: 4 },
+        position: { x: -1 },
+        material: {type: 'phong', color: 0x335588, opacity: 0.5, transparent: true, wireframe: true},
+        mass: 0,
+        connector: {
+          wA: {
+            base: {x: 1, y: 2, z: 2},
+            up: {x: 1},
+            front: {z: 1}
+          },
+          wB: {
+            base: {x: 1, y: -2, z: -2},
+            up: {x: 1},
+            front: {z: 1}
+          }
+        }
+      },
+      a: {
+        type: 'basic',
+        shape: { type: 'cylinder', r: 4, dy: 2, segments: 8},
+        position: {x: 1, y: 3, z: 3},
+        rotation: {z: -Math.PI/2},
+        material: {type: 'phong', color: 0x338855, opacity: 0.5, transparent: true},
+        mass: 1,
+        connector: {
+          'cA': {
+            base: {y: -1},
+            up: {y: 1},
+            front: {z: 1}
+          }
+        }
+      },
+      b: {
+        type: 'basic',
+        shape: { type: 'cylinder', r: 1, dy: 2, segments: 8 },
+        rotation: {z: Math.PI/2},
+        material: {type: 'phong', color: 0x885555, opacity: 0.5, transparent: true},
+        mass: 1,
+        connector: {
+          'cB': {
+            base: {y: -1},
+            up: {y: 1},
+            front: {z: 1}
+          }
+        }
+      }
+    },
+    constraint: {
+      motor: {
+        type: 'hinge',
+        a: 'wA',
+        b: 'cA',
+        bodyA: 'wall',
+        bodyB: 'a',
+        approach: true
+      },
+      wb: {
+        type: 'hinge',
+        a: 'wB',
+        b: 'cB',
+        bodyA: 'wall',
+        bodyB: 'b',
+        approach: true
+      },
+      gear: {
+        type: 'gear',
+        a: 'cA',
+        b: 'cB',
+        bodyA: 'a',
+        bodyB: 'b',
+        ratio: 0.25,
+        approach: false
+      }
+    },
+    scene: {s1: {}},
+    monitor: {m1: {camera: 'satellite', lookAt: 'wall', distance: 15}},
+    light: {
+      l1: {position: {x: 5, z: -5}},
+      l2: {position: {x: -7, y: 6, z: 5}, color: 0x8899bb},
+      l3: {position: {y: -5, z: 1}, color: 0x445566}
+    }
+  };
+
+  factory.loadScene(pack, {
+    axisHelper: 2,
+    wireframe: false,
+    webWorker: false,
+    autoStart: true,
+    connectorHelper: 0.7,
+    canvasContainer: '#container'
+  });
+
+  if (inputs[type]) {
+    inputs[type](type);
+  }
 }
 
 addAllTests();
+test['gear'] = gearTest;
+
 //test.all = utils.all(test, 1);
 module.exports.test = test;
 module.exports.clearObjects = clearObjects;
