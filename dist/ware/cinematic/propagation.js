3f4e12af94c4b8dcfdbdbe0997c16cfe5019c92a
nrox <nunuroxa@gmail.com>
2015-05-07 14:11:09 +0200

    start adding a second level to cinematic.js

--- dist/ware/cinematic/propagation.js
+++ dist/ware/cinematic/propagation.js
@@ -6,23 +6,26 @@ var defaultOptions = {
   beamThickness: 0.4,
   longBeamLength: 6,
   beamDistance: 1,
-  segmentMass: 0.1,
-  freeze: false
+  segmentMass: 0.01,
+  freeze: true,
+  axisHelper: 0,
+  connectorHelper: 0.5
 };
 
 var getObject = function (o) {
   _.defaults(o || {}, defaultOptions);
-
+  var gap = 0.01;
   var object = {
     settings: {
       local: {
         freeze: o.freeze,
-        axisHelper: false
+        axisHelper: o.axisHelper,
+        connectorHelper: o.connectorHelper
       }
     },
     shape: {
-      vertical: {type: 'box', dy: o.longBeamLength, dx: o.beamThickness, dz: o.beamThickness},
-      horizontal: {type: 'box', dy: o.beamThickness, dx: o.beamThickness, dz: 2 * o.beamDistance + o.beamThickness}
+      vertical: {type: 'box', dy: o.longBeamLength, dx: o.beamThickness, dz: o.beamThickness, gap: gap},
+      horizontal: {type: 'box', dy: o.beamThickness, dx: o.beamThickness, dz: 2 * o.beamDistance + o.beamThickness, gap: gap}
     },
     material: {
       red: {color: 0x883333, opacity: o.opacity},
@@ -32,7 +35,7 @@ var getObject = function (o) {
     },
     body: {
       vertical: {
-        shape: 'vertical', material: 'red', mass: 0, mask: '1', position: {y: o.longBeamLength / 2 - o.beamThickness / 2},
+        shape: 'vertical', material: 'red', mass: o.segmentMass, mask: '1', position: {y: o.longBeamLength / 2 - o.beamThickness / 2},
         connector: {
           top: {
             base: {y: o.longBeamLength / 2 - o.beamThickness / 2},
@@ -45,7 +48,7 @@ var getObject = function (o) {
         }
       },
       horizontal: {
-        shape: 'horizontal', material: 'blue', mass: o.segmentMass, mask: '10', position: {y: 0},
+        shape: 'horizontal', material: 'blue', mass: 0, mask: '10', position: {y: 0},
         connector: {
           center: {
             base: {},
@@ -58,16 +61,28 @@ var getObject = function (o) {
           right: {
             base: {z: o.beamDistance},
             up: {x: 1}, front: {y: -1}
+          },
+          far: {
+            base: {x: -o.beamThickness},
+            up: {x: 1}, front: {y: -1}
           }
         }
       },
       left: {type: 'copy', of: 'vertical', mass: o.segmentMass, position: {y: o.longBeamLength / 2 - o.beamThickness / 2, z: -o.beamDistance}},
       right: {type: 'copy', of: 'vertical', mass: o.segmentMass, position: {y: o.longBeamLength / 2 - o.beamThickness / 2, z: o.beamDistance}},
-      top: {type: 'copy', of: 'horizontal', mass: o.segmentMass, position: {y: o.longBeamLength - o.beamThickness}}
+      top: {type: 'copy', of: 'horizontal', mass: o.segmentMass, position: {y: o.longBeamLength - o.beamThickness}},
+      farBottom: {type: 'copy', of: 'horizontal', mass: o.segmentMass, position: {x: -o.beamThickness}},
+      farTop: {type: 'copy', of: 'horizontal', mass: o.segmentMass, position: {x: -o.beamThickness, y: o.longBeamLength - o.beamThickness}},
+      farLeft: {type: 'copy', of: 'vertical', mass: o.segmentMass, position: {x: -o.beamThickness, y: o.longBeamLength / 2 - o.beamThickness / 2, z: -o.beamDistance}},
+      farRight: {type: 'copy', of: 'vertical', mass: o.segmentMass, position: {x: -o.beamThickness, y: o.longBeamLength / 2 - o.beamThickness / 2, z: o.beamDistance}},
+      secondCenter: {type: 'copy', of: 'vertical', mass: o.segmentMass, position: {x: -o.beamThickness, y: 3 * o.longBeamLength / 2 - 3 * o.beamThickness / 2, z: 0}}
     },
     constraint: {
-      servo: {
+      servo0: {
         type: 'servo', bodyA: 'horizontal', bodyB: 'vertical', connectorA: 'center', connectorB: 'bottom'
+      },
+      servo1: {
+        type: 'servo', bodyA: 'horizontal', bodyB: 'farBottom', connectorA: 'far', connectorB: 'center'
       },
       topCenter: {
         type: 'hinge', bodyA: 'top', bodyB: 'vertical', connectorA: 'center', connectorB: 'top'
@@ -83,6 +98,24 @@ var getObject = function (o) {
       },
       topRight: {
         type: 'hinge', bodyA: 'top', bodyB: 'right', connectorA: 'right', connectorB: 'top'
+      },
+      farTop: {
+        type: 'hinge', bodyA: 'top', bodyB: 'farTop', connectorA: 'far', connectorB: 'center'
+      },
+      farBottomLeft: {
+        type: 'hinge', bodyA: 'farBottom', bodyB: 'farLeft', connectorA: 'left', connectorB: 'bottom'
+      },
+      farBottomRight: {
+        type: 'hinge', bodyA: 'farBottom', bodyB: 'farRight', connectorA: 'right', connectorB: 'bottom'
+      },
+      farTopLeft: {
+        type: 'hinge', bodyA: 'farTop', bodyB: 'farLeft', connectorA: 'left', connectorB: 'top'
+      },
+      farTopRight: {
+        type: 'hinge', bodyA: 'farTop', bodyB: 'farRight', connectorA: 'right', connectorB: 'top'
+      },
+      fixSecondCenter: {
+        type: 'fixed', bodyA: 'farTop', bodyB: 'secondCenter', connectorA: 'center', connectorB: 'bottom'
       }
     }
   };
@@ -96,14 +129,21 @@ var userInterface = function (options) {
     system: undefined,
     container: 'body'
   });
+  function serve(servo, angle) {
+    var c = this.rootSystem.getConstraint({system: options.system, id: servo});
+    c.setAngle(angle);
+  }
+
   return {
     values: {
       sweep: function () {
         var _this = this;
         var time = utils.seconds();
         setInterval(function () {
-          var c = _this.rootSystem.getConstraint({system: options.system, id: 'servo'});
-          c.setAngle(Math.sin(utils.seconds(time)));
+          _.each(['servo0', 'servo1'], function (servo, index) {
+            var angle = 0.75 * Math.sin((1 + index * 0.5) * utils.seconds(time));
+            serve.call(_this, servo, angle);
+          });
         }, 100);
       }
     },
